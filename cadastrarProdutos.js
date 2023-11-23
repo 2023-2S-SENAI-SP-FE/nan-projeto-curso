d80dc50bf34a5b24afa3446386a8e5f864593050
Juan Lima <110911412+jlima32@users.noreply.github.com>
2023-11-23 12:38:11 +0000

    feat: cadastro dos produtos

--- cadastrarProdutos.js
+++ cadastrarProdutos.js
@@ -4,16 +4,21 @@ function postData(){
 
     const data = {
         nome: document.getElementById('nome').value,
-        preco: document.getElementById('preco').value
+        preco: document.getElementById('preco').value,
+        descricao: document.getElementById('descricao').value,
+        categoria: document.getElementById('categoria').value,
+        img1: document.getElementById('img1').value,
+        img2: document.getElementById('img2').value,
+        img3: document.getElementById('img3').value,
+        avaliacao: 0,
+        status: 'disponível',
     }
     
-    const response = fetch('https://655f35cd879575426b44d699.mockapi.io/api/produtos/produtos', {
+    const response = fetch('https://655f44c1879575426b44f818.mockapi.io/api/produtos', {
         method: 'POST',
         headers: { 
             "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
     })
-
-    console.log(data);
 }
