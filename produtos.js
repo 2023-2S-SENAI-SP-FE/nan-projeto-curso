de10daad0efe324d067cac26ea16fe36d2350b7f
Juan Lima <juangustavo@gmail.com>
2023-11-25 12:54:40 -0300

    feat: editar produtos

--- produtos.js
+++ produtos.js
@@ -1,4 +1,4 @@
-
+let produtosData;
 
 function cadastrarProduto(){
 
@@ -29,12 +29,13 @@ function cadastrarProduto(){
 
 
 function listarProdutos() {
+    limparFormulario();
     document.getElementById('listaProdutos').innerHTML = '';
     fetch('https://655f44c1879575426b44f818.mockapi.io/api/produtos')
         .then((response) => response.json())
         .then((produtos) => {produtosData = produtos
         for (i = 0; i < produtosData.length; i++){
-            document.getElementById('listaProdutos').innerHTML += `<p class="linhas">Produto: ${produtosData[i].nome} || Preço: ${produtosData[i].preco} || Categoria: ${produtosData[i].categoria} <button type="button" class="btnExcluir" onclick=excluirProduto(${produtosData[i].id})>Excluir</button> </p>`
+            document.getElementById('listaProdutos').innerHTML += `<p class="linhas">Produto: ${produtosData[i].nome} || Preço: ${produtosData[i].preco} || Categoria: ${produtosData[i].categoria} || <button type="button" class="btnExcluir" onclick=excluirProduto(${produtosData[i].id})>Excluir</button> || <button type="button" class="btnExcluir" onclick=listarIndividual(${produtosData[i].id})>Editar</button></p>`
         }
         })
 
@@ -57,3 +58,42 @@ function excluirProduto(id){
 }
 
 listarProdutos();
+
+function editarProduto(id){
+    
+    let Altdata = {
+        nome: document.getElementById('nome').value,
+        preco: document.getElementById('preco').value,
+        descricao: document.getElementById('descricao').value,
+        categoria: document.getElementById('categoria').value,
+        img1: document.getElementById('img1').value,
+        img2: document.getElementById('img2').value,
+        img3: document.getElementById('img3').value
+    }
+    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/produtos/${id}`, {
+        method: 'PUT',
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(Altdata)
+    })
+    alert('Produto editado com sucesso!')
+    limparFormulario();
+    listarProdutos();
+}
+
+function listarIndividual(id){
+    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/produtos/${id}`)
+        .then((response) => response.json())
+        .then((produto) => {
+            document.getElementById('nome').value = produto.nome
+            document.getElementById('preco').value = produto.preco
+            document.getElementById('descricao').value = produto.descricao
+            document.getElementById('categoria').value = produto.categoria
+            document.getElementById('img1').value = produto.img1
+            document.getElementById('img2').value = produto.img2
+            document.getElementById('img3').value = produto.img3
+        })
+    document.getElementById('botoes').innerHTML = `<button onclick='editarProduto(${id})'>Editar</button>  `
+        
+}
