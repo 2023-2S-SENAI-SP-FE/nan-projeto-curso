7911a64ebc15672d0c5b62071246f1b8c39a8e91
juliana <jrtokura@gmail.com>
2023-12-12 12:39:44 +0000

    feat: criou e organizou botoes

--- cadastro.js
+++ cadastro.js
@@ -1,26 +1,35 @@
 
 // buscar dados na API ou em qualquer lugar
-function listarProdutos () {
+function listarCategorias () {
     
     document.getElementById("categorias").innerHTML ="";
     fetch ("https://655f44c1879575426b44f818.mockapi.io/api/categorias")
     .then((response)=>(response.json()))
-    .then((categoria)=> {
-        categorias.forEach((categoria) => {
-            document.getElementById ("categorias").innerHTML += `<p>${categoria[i].nome}</p>`
+    .then((categorias)=> {
+        categorias.forEach((categoria)=> {
+            document.getElementById ("categorias").innerHTML += `<p>${categoria.nome}/<button onclick="deleteProdutos(${categoria.id})">Excluir</button><button onclick="editarCategorias(${categoria.id})">Editar</button></p>`  
         })
     })  
 }
 
-listarProdutos() 
+listarCategorias() 
+
+function editarCategorias (id){
+    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/categorias/${id}`)
+    .then((response)=>(response.json()))
+    .then((categoria)=> {
+        document.getElementById("nome").value = categoria.nome;
+    })
+    document.getElementById("botoes").innerHTML = `<button onclick=altProd(${id})>editar</button>`
+} 
 
 function cadastrarCategorias(){
     let dados = {
         nome:document.getElementById('nome').value,
-        categoria: document.getElementById("categoria").value,
-        nome: document.getElementById("nome").value,
-        produto: document.getElementById("produto").value,
-        preco:document.getElementById('preco').value,
+        // categoria: document.getElementById("categoria").value,
+        // nome: document.getElementById("nome").value,
+        // produto: document.getElementById("produto").value,
+        // preco:document.getElementById('preco').value,
         }
 
     fetch('https://655f44c1879575426b44f818.mockapi.io/api/categorias',{
@@ -37,13 +46,13 @@ function cadastrarCategorias(){
 
 // cadastrarCategorias ()
 
-function limparFormulario () {
+function limparFormulario(){
     var campos = document.querySelectorAll('.form-value');
     campos.forEach(campo => campo.value = '');
 }
 
 function deleteProdutos(id) {
-    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/categorias`,{
+    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/categorias/${id}`,{
         method:'DELETE',
         headers: {'Content-Type':"application/json"}
     })
@@ -54,12 +63,12 @@ function deleteProdutos(id) {
 function altProd(id){
     let editado = {
         nome:document.getElementById('nome').value,
-        categoria: document.getElementById("categoria").value,
-        nome: document.getElementById("nome").value,
-        produto: document.getElementById("produto").value,
-        preco:document.getElementById('preco').value,
+        // categoria: document.getElementById("categoria").value,
+        // nome: document.getElementById("nome").value,
+        // produto: document.getElementById("produto").value,
+        // preco:document.getElementById('preco').value,
     }
-    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/ ${id}`,{
+    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/categorias/${id}`,{
         method:'PUT',
         headers: {"content-type": "application/json"
         }, //=> define o tipo do conteúdo da requisição como JSON, isso é necessário porque os dados enviado estão em formato JSON
