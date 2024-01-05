459ba555e499ebb191eebed32ef78456f2310bc4
Juan Lima <juangustavo@gmail.com>
2024-01-05 10:53:21 -0300

    feat: listar categorias

--- admin/script.js
+++ admin/script.js
@@ -1,3 +1,5 @@
+listarCategorias();
+
 function cadastrarCategoria(){
 
     const data = {
@@ -13,5 +15,26 @@ function cadastrarCategoria(){
     })
 
     alert(`Categoria adicionada com sucesso!`)
+    listarCategorias();
 
+}
+
+function listarCategorias(){
+    document.querySelector('.table-content').innerHTML = '';
+    fetch('https://655f44c1879575426b44f818.mockapi.io/api/categorias')
+        .then((response) => response.json())
+        .then((categoria) => {
+            for (i = 0; i < categoria.length; i++){
+                document.querySelector('.table-content').innerHTML += `
+            <div class="table-row">
+                <div class="table-data">${categoria[i].id}</div>
+                <div class="table-data">${categoria[i].nome}</div>
+                <div class="table-data edit">
+                    <div class="btnEdit">Editar</div>
+                    <div class="btnExcluir">Excluir</div>
+                </div>
+            </div>
+                `;
+            }
+        })
 }
