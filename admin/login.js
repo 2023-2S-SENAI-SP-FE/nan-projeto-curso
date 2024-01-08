1ffc7af9cf0295e90ac79ca96870335aec64bc90
Juan Lima <juangustavo@gmail.com>
2024-01-08 11:28:26 -0300

    feat: login ok

---
+++ admin/login.js
@@ -0,0 +1,59 @@
+let listaUsuarios = [];
+listarUsuarios();
+
+async function listarUsuarios(){
+    await fetch(`https://65995a81a20d3dc41cef99fb.mockapi.io/usuarios`)
+        .then((response) => response.json())
+        .then((usuarios) => {
+            listaUsuarios = usuarios;
+        })
+}
+function login(){
+
+
+    let email = document.querySelector('#email');
+    let emailLabel = document.querySelector('#emailLabel');
+    let senha = document.querySelector('#senha');
+    let senhaLabel = document.querySelector('#senhaLabel');
+
+    let msgError = document.querySelector('#msgError');
+   
+
+    let validaUsuario = {
+        nome: '',
+        email: '',
+        senha: '',
+        nivel: ''
+    }
+
+    listaUsuarios.forEach((item) => {
+        if(email.value == item.email && sha256(senha.value) == item.senha){
+            validaUsuario = {
+                nome: item.nome,
+                email: item.email,
+                senha: item.senha,
+                nivel: item.nivel
+            }
+           
+        }
+    })
+
+    if(email.value == validaUsuario.email && sha256(senha.value) == validaUsuario.senha){
+        if(validaUsuario.nivel > 1){
+             window.location.href = './index.html';
+             let token = Math.random().toString(16).substr(2);
+             localStorage.setItem('token', token);
+             localStorage.setItem('usuarioLogado', JSON.stringify(validaUsuario))
+        }else{
+            alert('Você não pode acessar essa área <p> redirecionando para a loja... </p>');  
+        }
+       
+    }else{
+        emailLabel.setAttribute('style', 'color: red');
+        senhaLabel.setAttribute('style', 'color: red');
+        msgError.setAttribute('style', 'display: block');
+        msgError.innerHTML = 'Usuário ou senha inválida';
+        email.focus();
+    }
+
+}
