17331d0ca390750754f605fe3d99b1414d5cd352
Erick Hanon <erickheinom@gmail.com>
2024-01-10 11:55:11 -0300

    feat: Interação da página do carrinho com a págida dos Meus Pedidos

---
+++ valorprodutos.js
@@ -0,0 +1,13 @@
+function valorProduto() {
+
+Totalcompra = document.querySelector("#preco");
+
+    fetch ("produtoSite.json").then((response) => {
+    response.json().then((total) =>{
+        
+    })
+    }) 
+
+}
+
+
