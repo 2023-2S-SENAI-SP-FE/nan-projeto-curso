d818c48be8953ac68fefbab2d03b3ce9f72b43fb
Juan Lima <juangustavo@gmail.com>
2024-01-11 14:37:37 -0300

    feat: menu com categorias

---
+++ js/categorias.js
@@ -0,0 +1,14 @@
+function listarCategorias(){
+    let ulCat = document.getElementById('listaCategorias');
+    fetch(`https://655f44c1879575426b44f818.mockapi.io/api/categorias/?orderby=nome`)
+        .then((response) => response.json())
+        .then((categoria) => {
+            categoria.forEach(item => {
+                let li = document.createElement('li');
+                li.innerHTML = `<a href='#'>${item.nome}</a>`;
+                ulCat.appendChild(li);
+            });
+        })
+}
+
+listarCategorias();
