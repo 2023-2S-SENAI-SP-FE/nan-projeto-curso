5fc16e2b8aca05d2d0b5259ce77ebfbe4c368362
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2024-01-11 14:03:48 +0000

    fix: Correção na quantidade de produtos relacionadso

--- pagina-produto.js
+++ pagina-produto.js
@@ -1,4 +1,4 @@
-dadosProduto(1)
+dadosProduto(3)
 
 function dadosProduto(id) {
     fetch(`https://655f44c1879575426b44f818.mockapi.io/api/produtos/${id}`, {
@@ -7,77 +7,71 @@ function dadosProduto(id) {
             'Content-type': 'application/json'
         }
     })
-        .then(resposta => resposta.json())
-        .then(resposta => {
-            produto = resposta;
-
+        .then(produto => produto.json())
+        .then(produto => {
             document.querySelector('#paginaProduto').innerHTML = `   
-            <section class="paginaProdutoPrincipal">
-            <div class="paginaProdutoPrincipalProduto">
-                <div class="paginaProdutoPrincipalProdutoMiniaturas">
-                <!--    <figure><img width="80px" height="80px" src="${produto.img1}" alt="imagem 1 do  ${produto.nome}"></figure> -->
-                    <figure><img src="${produto.img2}" alt="imagem 2 do  ${produto.nome}"></figure>
-                    <figure><img src="${produto.img3}" alt="imagem 3 do  ${produto.nome}"></figure>
+                <section class="paginaProdutoPrincipal">
+                <div class="paginaProdutoPrincipalProduto">
+                    <div class="paginaProdutoPrincipalProdutoMiniaturas">
+                    <!--    <figure><img width="80px" height="80px" src="${produto.img1}" alt="imagem 1 do  ${produto.nome}"></figure> -->
+                        <figure><img src="${produto.img2}" alt="imagem 2 do  ${produto.nome}"></figure>
+                        <figure><img src="${produto.img3}" alt="imagem 3 do  ${produto.nome}"></figure>
+                    </div>
+                    <div class="paginaProdutoPrincipalProdutoFoto">
+                        <figure><img src="${produto.img1}" alt="imagem 1 do  ${produto.nome}"></figure>
+                    </div>
+                    <div class="paginaProdutoPrincipalProdutoDescricao">
+                        <div class="paginaProdutoPrincipalProdutoDescricaoC">
+                            <h2>${produto.nome}</h2>
+                            <div class="paginaProdutoPrincipalProdutoDescricaoEstrelas">
+                                <figure><img src="./Star 4.png" alt=""></figure>
+                                <figure><img src="./Star 4.png" alt=""></figure>
+                                <figure><img src="./Star 4.png" alt=""></figure>
+                                <figure><img src="./Star 4.png" alt=""></figure>
+                                <figure><img src="./Star 5.png" alt=""></figure>
+                            </div>
+                        </div>
+
+                        <div class="paginaProdutoPrincipalProdutoDescricaoC">
+                            <p>R$ ${produto.preco}</p>
+                            <p>Em até 6x de R$ ${(produto.preco / 6).toFixed(2)} sem juros </p>
+                        </div>
+                        <p> ${produto.descricao}</p>
+                    </div>
                 </div>
-                <div class="paginaProdutoPrincipalProdutoFoto">
-                    <figure><img src="${produto.img1}" alt="imagem 1 do  ${produto.nome}"></figure>
-                </div>
-                <div class="paginaProdutoPrincipalProdutoDescricao">
-                    <div class="paginaProdutoPrincipalProdutoDescricaoC">
-                        <h2>${produto.nome}</h2>
-                        <div class="paginaProdutoPrincipalProdutoDescricaoEstrelas">
-                            <figure><img src="./Star 4.png" alt=""></figure>
-                            <figure><img src="./Star 4.png" alt=""></figure>
-                            <figure><img src="./Star 4.png" alt=""></figure>
-                            <figure><img src="./Star 4.png" alt=""></figure>
-                            <figure><img src="./Star 5.png" alt=""></figure>
+                    <div id="paginaProdutoPrincipalRelacionados"> </div>
+                    <div class="paginaProdutoPrincipalDescricao">
+                        <h3>Descrição do Produto</h3>
+                        <p>${produto.descricao}</p>
+                    </div>
+                </section>
+                <aside class="paginaProdutoAside">
+                    <div class="paginaProdutoAsideInteracao">
+                        <form action="" class="paginaProdutoAsideInteracaoComprar">
+                            <p>Status do produto: ${produto.status} </p>
+                            <div>
+                                <label for="quantidadeProduto">Quantidade: </label>
+                                <select name="quantidadeProduto" id="quantidadeProduto">
+                                    <option value="1">1</option>
+                                    <option value="10">10</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                            </div>
+                            <button type="submit">COMPRAR</button>
+                            <button type="submit">Adicionar ao carrinho</button>
+                        </form>
+
+                        <div action="" class="paginaProdutoAsideInteracaoFrete" id = "paginaProdutoAsideInteracaoFrete">
+                            <!-- <h3>Frete</h3> -->
+                            <input type="text" name="CEP" id="CEP" placeholder="00000000-0">
+                            <button id="calcularFrete">Calcular frete</button>
                         </div>
                     </div>
-
-                    <div class="paginaProdutoPrincipalProdutoDescricaoC">
-                        <p>R$ ${produto.preco}</p>
-                        <p>Em até 6x de R$ ${(produto.preco / 6).toFixed(2)} sem juros </p>
-                    </div>
-
-                    <p> ${produto.descricao}</p>
-
-                </div>
-                </div>
-                <div id="paginaProdutoPrincipalRelacionados">       
-
-                </div>
-                <div class="paginaProdutoPrincipalDescricao">
-                    <h3>Descrição do Produto</h3>
-                    <p>${produto.descricao}</p>
-                </div>
-            </section>
-            <aside class="paginaProdutoAside">
-                <div class="paginaProdutoAsideInteracao">
-                    <form action="" class="paginaProdutoAsideInteracaoComprar">
-                        <p>Status do produto: ${produto.status} </p>
-                        <div>
-                            <label for="quantidadeProduto">Quantidade: </label>
-                            <select name="quantidadeProduto" id="quantidadeProduto">
-                                <option value="1">1</option>
-                                <option value="10">10</option>
-                                <option value="50">50</option>
-                                <option value="100">100</option>
-                            </select>
-                        </div>
-                        <button type="submit">COMPRAR</button>
-                        <button type="submit">Adicionar ao carrinho</button>
-                    </form>
-
-                    <div action="" class="paginaProdutoAsideInteracaoFrete" id = "paginaProdutoAsideInteracaoFrete">
-                        <!-- <h3>Frete</h3> -->
-                        <input type="text" name="CEP" id="CEP" placeholder="00000000-0">
-                        <button id="calcularFrete">Calcular frete</button>
-                    </div>
-                </div>
-                <div class="paginaProdutoAsideEspaçoVago1"></div>
-                <div class="paginaProdutoAsideEspaçoVago2"></div>
-            </aside>
-        </article>
+                    <div class="paginaProdutoAsideEspaçoVago1"></div>
+                    <div class="paginaProdutoAsideEspaçoVago2"></div>
+                </aside>
+            </article>
             `})
 
         .then(respostarelacionado => {
@@ -85,32 +79,30 @@ function dadosProduto(id) {
             for (let index = 0; index <= 2; index++) {
               id[index] = idRel()
 
-                fetch(`https://655f44c1879575426b44f818.mockapi.io/api/produtos/${id[index]}`, {
+              fetch(`https://655f44c1879575426b44f818.mockapi.io/api/produtos/${id[index]}`, {
                     method: 'GET',
                     headers: {
                         'Content-type': 'application/json'
                     }
                 })
 
-                    .then(respostarelacionado => respostarelacionado.json())
-                    .then(respostarelacionado => {
-
-                        produtoRelecionado = respostarelacionado;
+                    .then(produtoRelecionado => produtoRelecionado.json())
+                    .then(produtoRelecionado => {
 
                         // console.log(produtoRelecionado)
-
-                        document.querySelector('#paginaProdutoPrincipalRelacionados').innerHTML += `
-                    <div class="paginaProdutoPrincipalRelacionadosProdutos" >
-                
-                        <figure><img width="96px" height="96px" src="${produtoRelecionado.img1}" alt="imagem do  ${produtoRelecionado.nome}"></figure>
-                        <p> ${produtoRelecionado.nome}</p>
-                        <p>R$ ${produtoRelecionado.preco}</p>
-                        <p>Em até 6 x R$ ${(produtoRelecionado.preco / 6).toFixed(2)} sem juros </p>
-                        <button type="submit" id="${id[index]}">COMPRAR</button>
-
-                    </div>
-                    `})
-                    .then(relCarregado => {
+ 
+                        if(id.length <= 3){
+                            document.querySelector('#paginaProdutoPrincipalRelacionados').innerHTML += `
+                                <div class="paginaProdutoPrincipalRelacionadosProdutos" >
+                                    <figure><img width="96px" height="96px" src="${produtoRelecionado.img1}" alt="imagem do  ${produtoRelecionado.nome}"></figure>
+                                    <p> ${produtoRelecionado.nome}</p>
+                                    <p>R$ ${produtoRelecionado.preco}</p>
+                                    <p>Em até 6 x R$ ${(produtoRelecionado.preco / 6).toFixed(2)} sem juros </p>
+                                    <button type="submit" id="${id[index]}">COMPRAR</button>
+                                </div>
+                    `}
+                })
+                    .then(() => {
                         document.getElementById(id[0]).addEventListener('click', () => {
                             dadosProduto(id[0])
                         })
@@ -133,14 +125,8 @@ function dadosProduto(id) {
                     <p> Freté é R$ ${(calcularFrete(document.querySelector("#CEP").value)).toFixed(2)}</p>
                     
                     
-                    `
-                    
-                })
-
-            
-
+                    ` })         
             })
-
 }
 
 
@@ -155,11 +141,8 @@ function idRel(idrelacionado) {
 function calcularFrete(cep) {
     valorFrete = 0
     valorFrete = (cep * 500) / 899999999
-    console.log("Valor frete = " + valorFrete)
-
+    // console.log("Valor frete = " + valorFrete)
     return valorFrete
-
-
 }
 
 
